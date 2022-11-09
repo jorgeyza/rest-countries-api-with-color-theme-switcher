132d5daa4c66d278ef733ff54a2dfd8a46eb7105
Jorge <jorgeyza@gmail.com>
2022-11-09 18:06:40 -0500

    Add RegionsFilter

---
+++ app/components/RegionsFilter.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { useState } from 'react';
+import { Button, Menu, MenuButton, MenuItem, MenuList, Text } from '@chakra-ui/react';
+import { FaChevronDown } from 'react-icons/fa';
+
+const regions = [
+  { label: 'Africa', value: 'africa' },
+  { label: 'America', value: 'america' },
+  { label: 'Asia', value: 'asia' },
+  { label: 'Europe', value: 'europe' },
+  { label: 'Oceania', value: 'oceania' }
+];
+
+const RegionsFilter = () => {
+  const [selectedRegion, setSelectedRegion] = useState<string | null>(null);
+
+  const handleMenuItemClick = (value: string) => {
+    setSelectedRegion(value);
+  };
+
+  return (
+    <Menu>
+      <MenuButton
+        as={Button}
+        width='100%'
+        maxWidth={200}
+        backgroundColor='dm.darkBlue'
+        rightIcon={<FaChevronDown size={12} />}>
+        {regions.find((region) => region.value === selectedRegion)?.label ?? (
+          <Text as='span' fontWeight={300}>
+            Filter by Region
+          </Text>
+        )}
+      </MenuButton>
+      <MenuList>
+        {regions.map((region) => (
+          <MenuItem key={region.value} onClick={() => handleMenuItemClick(region.value)}>
+            {region.label}
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default RegionsFilter;
