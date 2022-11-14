7c8e28186c81686e60f0aca06faabf5421ca0d56
Jorge <jorgeyza@gmail.com>
2022-11-14 17:13:55 -0500

    Update loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,11 @@
-const Loading = () => {
-  return <div>Loading...</div>;
-};
+'use client';
 
-export default Loading;
+import { Box, Spinner } from '@chakra-ui/react';
+
+export default function Loading() {
+  return (
+    <Box display='flex' justifyContent='center' alignItems='center' height='90vh'>
+      <Spinner thickness='4px' speed='0.65s' emptyColor='gray.200' color='blue.500' size='xl' />
+    </Box>
+  );
+}
