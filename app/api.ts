f8bb6a1055740d777ee52fb45b5a5a1f557e693a
Jorge <jorgeyza@gmail.com>
2022-11-18 17:52:52 -0500

    Add getCountryByName function

--- app/api.ts
+++ app/api.ts
@@ -5,6 +5,11 @@ export const getAllCountries = async (): Promise<ICountry[]> => {
   return await response.json();
 };
 
+export const getCountryByName = async (countryName: string): Promise<ICountry[]> => {
+  const response = await fetch(`https://restcountries.com/v3.1/name/${countryName.toLowerCase()}`);
+  return await response.json();
+};
+
 export const getCountriesByCode = async (countryCodes: string[]): Promise<ICountry[]> => {
   const response = await fetch(`https://restcountries.com/v3.1/alpha?codes=${countryCodes.join(',')}`);
   return await response.json();
