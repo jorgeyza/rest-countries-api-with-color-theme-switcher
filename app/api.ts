46d34de72bf2a2a6080cf42803e684df72807e44
Jorge <jorgeyza@gmail.com>
2022-12-15 23:18:15 -0500

    Add error handling

--- app/api.ts
+++ app/api.ts
@@ -2,15 +2,24 @@ import { ICountry } from './types';
 
 export const getAllCountries = async (): Promise<ICountry[]> => {
   const response = await fetch('https://restcountries.com/v3.1/all');
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
   return await response.json();
 };
 
-export const getCountryByName = async (countryName: string): Promise<ICountry[]> => {
+export const getCountryByName = async (countryName: string): Promise<ICountry> => {
   const response = await fetch(`https://restcountries.com/v3.1/name/${countryName.toLowerCase()}`);
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
   return await response.json();
 };
 
 export const getCountriesByCode = async (countryCodes: string[]): Promise<ICountry[]> => {
   const response = await fetch(`https://restcountries.com/v3.1/alpha?codes=${countryCodes.join(',')}`);
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
   return await response.json();
 };
